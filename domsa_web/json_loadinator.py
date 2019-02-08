2949f506473ed15d37111fb9631637ddc56e51ed
mitchya1 <mitchell.anderson@colorado.edu>
2019-02-08 15:30:30 -0700

    add input_rating in powersupply func

--- domsa_web/json_loadinator.py
+++ domsa_web/json_loadinator.py
@@ -42,15 +42,15 @@ def PowerSupplies(json_object):
         for i in json_object['Report']:
                 ac_on = json_object['Report'][i]['ACOn']
                 name = json_object['Report'][i]['Name']
-                #input_rating = json_object['Report'][i]['InputRating']
+                input_rating = json_object['Report'][i]['InputRating']
                 fan_ok = json_object['Report'][i]['FanFailed']
                 fw_ver = json_object['Report'][i]['FirmwareVersion']
                 exists = json_object['Report'][i]['Detected']
                 failed = json_object['Report'][i]['Failed']
                 predict_fail = json_object['Report'][i]['PredictedFail']
                 ac_status = json_object['Report'][i]['ACLost']
-                #mongo_doc = {"Name": name, "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_ok, "FirmwareVersion": fw_ver, "ACOn": ac_on}
-                mongo_doc = {"Name": name, "Detected": exists, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_ok, "FirmwareVersion": fw_ver, "ACOn": ac_on}
+                mongo_doc = {"Name": name, "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_ok, "FirmwareVersion": fw_ver, "ACOn": ac_on}
+                #mongo_doc = {"Name": name, "Detected": exists, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_ok, "FirmwareVersion": fw_ver, "ACOn": ac_on}
                 host_collection = db[host].insert_one(mongo_doc)
 
 """
