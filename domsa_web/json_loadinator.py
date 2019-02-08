53dc95321f28b3dcaf021aea4649c39d30c34998
mitchya1 <mitchell.anderson@colorado.edu>
2019-02-08 14:42:42 -0700

    load json in routes

--- domsa_web/json_loadinator.py
+++ domsa_web/json_loadinator.py
@@ -1,7 +1,9 @@
 import json
 from pymongo import MongoClient
-from domsa_web import db, app
+from domsa_web import db, app, client
 #from flask_mongoalchemy import MongoAlchemy
+
+
 
 def Temperature(json_object):
     host = json_object['Host']
@@ -10,13 +12,39 @@ def Temperature(json_object):
         TempInC = json_object['Report'][i]['TempInC']
         mongo_doc = {"Host": host, "Category": "Temperature", "TempReading": TempReading, "TempInC": TempInC}
         host_collection = db[host].insert_one(mongo_doc)
-"""
-def Memory(json_object):
 
-def Processors(json_object):
+def Memory(json_object):
+        host = json_object['Host']
+        for i in json_object['Report']:
+                dev_location = i
+                manufacturer = json_object['Report'][i]['Manufacturer']
+                serial  = json_object['Report'][i]['SerialNumber']
+                part_number = json_object['Report'][i]['PartNumber']
+                speed = json_object['Report'][i]['Speed']
+                size = json_object['Report'][i]['Size']
+                status = json_object['Report'][i]['Status']
+                mongo_doc = {"Host": host, "Device": dev_location, "Manufacturer": manufacturer, "SerialNumber": serial, "PartNumber": part_number, "Speed": speed, "Size": size, "Status": status}
+                host_collection = db[host].insert_one(mongo_doc)
+
+#def Processors(json_object):
 
 def PowerSupplies(json_object):
+        host = json_object['Host']
+        for i in json_object['Report']:
+                ac_on = json_object['Report'][i]['ACOn']
+                name = json_object['Report'][i]['Name']
+                #input_rating = json_object['Report'][i]['InputRating']
+                fan_ok = json_object['Report'][i]['FanFailed']
+                fw_ver = json_object['Report'][i]['FirmwareVersion']
+                exists = json_object['Report'][i]['Detected']
+                failed = json_object['Report'][i]['Failed']
+                predict_fail = json_object['Report'][i]['PredictedFail']
+                ac_status = json_object['Report'][i]['ACLost']
+                #mongo_doc = {"Name": name, "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_ok, "FirmwareVersion": fw_ver, "ACOn": ac_on}
+                mongo_doc = {"Name": name, "Detected": exists, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_ok, "FirmwareVersion": fw_ver, "ACOn": ac_on}
+                host_collection = db[host].insert_one(mongo_doc)
 
+"""
 def PhysicalDisks(json_object):
 
 def Fans(json_object):
@@ -24,5 +52,4 @@ def Fans(json_object):
 def NICs(json_object):
 
 def VirtDisk(json_object):
-
 """
--- domsa_web/routes.py
+++ domsa_web/routes.py
@@ -1,7 +1,7 @@
 from domsa_web import app, json_loadinator
 from flask import render_template, request
 from flask_mongoalchemy import MongoAlchemy
-import json
+import json, jsonify
 """
 for i in b['Report']:
      print(b['Report'][i]['Description'])
@@ -13,7 +13,32 @@ db.collection.find().sort({'_id':-1}).limit(1)
 
 @app.route('/api/report', methods=['POST'])
 def report_api():
-    if request.json:
-        category = request['Category']
-
-
+     if request.json:
+          #j = json.loads(request.json)
+          print(request.json)
+          #category = request.get_json()
+          request_json = request.json
+          print(request_json['Host'])
+          print(request_json['Category'])
+          if category == "Temperature":
+               json_loadinator.Temperature(request_json)     
+          elif category == "Memory":
+               json_loadinator.Memory(request_json)
+          elif category == "Processors":
+               json_loadinator.Processors(request_json)
+          elif category == "PowerSupplies":
+               json_loadinator.PowerSupplies(request_json)
+          elif category == "PhysicalDisks":
+               json_loadinator.PhysicalDisks(request_json)
+          elif category == "Fans": 
+               json_loadinator.VirtDisk(request_json)
+          elif category == "NICs":
+               json_loadinator.NICs(request_json)
+          elif category == "VirtDisk":
+               json_loadinator.VirtDisk(request_json)
+          else:
+               return "Invalid Category"
+          return "Passing it into a higher power"
+     else:
+          return "Invalid json"
+     return "Hello"
