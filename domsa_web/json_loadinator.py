1df3e8755dd6d38752d8790bd57abfd486772fe4
mitchya1 <mitchell.anderson@colorado.edu>
2019-02-11 14:04:27 -0700

    processors, not tested

--- domsa_web/json_loadinator.py
+++ domsa_web/json_loadinator.py
@@ -14,7 +14,9 @@ def Temperature(json_object):
         if TempInC > 42.0 or TempInC < 8.0:
                 alert_search = host_collection.find({'Category': 'Temperature'}, sort=[('_id', -1)], limit=1)
                 if alert_search.count() == 0:
-                        mongo_doc = {"Host": host, "Category": "Temperature", "TempReading": TempReading, "TempInC": TempInC, "Alert": "False"}
+                        slack_alert_message = "Board on {} is {} degrees celsius".format(host, str(TempInC))
+                        slack.alert("Temperature", slack_alert_message)
+                        mongo_doc = {"Host": host, "Category": "Temperature", "TempReading": TempReading, "TempInC": TempInC, "Alert": "True"}
                         host_collection.insert(mongo_doc)
                 else:
                         for item in alert_search:
@@ -46,11 +48,13 @@ def Memory(json_object):
                 size = json_object['Report'][i]['Size']
                 status = json_object['Report'][i]['Status']
                 host_collection = db[host]
-                if status != "0":    
+                if status != 0:    
                         alert_search = host_collection.find({"Category": "Memory", "Device": i}, sort=[('_id', -1)], limit=1)
                         if alert_search.count() == 0:
-                                # This document doesn't exist yet
-                                mongo_doc = {"Host": host, "Category": "Memory", "Device": dev_location, "Manufacturer": manufacturer, "SerialNumber": serial, "PartNumber": part_number, "Speed": speed, "Size": size, "Status": status, "Alert": "False"}
+                                slack_alert_message = "Status is not 0 for {} in {}".format(dev_location, host)
+                                slack.alert("Memory", slack_alert_message)
+                                # This document doesn't exist yet, create it
+                                mongo_doc = {"Host": host, "Category": "Memory", "Device": dev_location, "Manufacturer": manufacturer, "SerialNumber": serial, "PartNumber": part_number, "Speed": speed, "Size": size, "Status": status, "Alert": "True"}
                                 host_collection.insert(mongo_doc)
                         else:
                                 for item in alert_search:
@@ -92,8 +96,10 @@ def Processors(json_object):
                 if status != 3:
                         alert_search = host_collection.find({"Category": "Processors", "Device": proc_name}, sort=[('_id', -1)], limit=1)
                         if alert_search.count() == 0:
-                                mongo_doc = {"ProcessorName": proc_name, "MaxSpeed": max_speed, "CurrentSpeed": curr_speed, "Status": status, \
-                                        "Manufacturer": manufacturer, "Version": version, "Cores": cores, "Threads": thread_count, "Alert": "False"}
+                                slack_alert_message = "Status is not 0 for {} in {}".format(proc_name, host)
+                                slack.alert("Processors", slack_alert_message)
+                                mongo_doc = {"ProcessorName": proc_name, "Category": "Processors", "MaxSpeed": max_speed, "CurrentSpeed": curr_speed, "Status": status, \
+                                        "Manufacturer": manufacturer, "Version": version, "Cores": cores, "Threads": thread_count, "Alert": "True"}
                                 host_collection.insert(mongo_doc)
                         else:
                                 for item in alert_search:
@@ -101,13 +107,22 @@ def Processors(json_object):
                                                 try:
                                                         slack_alert_message = "Status is not 0 for {} in {}".format(proc_name, host)
                                                         slack.alert("Processors", slack_alert_message)
-                                                        mongo_doc = {"ProcessorName": proc_name, "MaxSpeed": max_speed, "CurrentSpeed": curr_speed, "Status": status, "Manufacturer": manufacturer, \
+                                                        mongo_doc = {"ProcessorName": proc_name, "Category": "Processors", "MaxSpeed": max_speed, "CurrentSpeed": curr_speed, "Status": status, "Manufacturer": manufacturer, \
                                                                 "Version": version, "Cores": cores, "Threads": thread_count, "Alert": "True"}
+                                                        host_collection.insert(mongo_doc)
                                                 except Exception as e:
                                                         return str(e)
+                                        else:
+                                                # Keep alert status set to true
+                                                mongo_doc = {"ProcessorName": proc_name, "Category": "Processors", "MaxSpeed": max_speed, "CurrentSpeed": curr_speed, "Status": status, "Manufacturer": manufacturer, \
+                                                        "Version": version, "Cores": cores, "Threads": thread_count, "Alert": "True"}
+                                                host_collection.insert(mongo_doc)
 
 
-                        print("he")
+                else:
+                        mongo_doc = {"ProcessorName": proc_name, "Category": "Processors", "MaxSpeed": max_speed, "CurrentSpeed": curr_speed, "Status": status, "Manufacturer": manufacturer, \
+                                "Version": version, "Cores": cores, "Threads": thread_count, "Alert": "False"}
+                        host_collection.insert(mongo_doc)
 
 
 def PowerSupplies(json_object):
@@ -116,7 +131,7 @@ def PowerSupplies(json_object):
                 ac_on = json_object['Report'][i]['ACOn']
                 name = json_object['Report'][i]['Name']
                 input_rating = json_object['Report'][i]['InputRating']
-                fan_ok = json_object['Report'][i]['FanFailed']
+                fan_failed = json_object['Report'][i]['FanFailed']
                 fw_ver = json_object['Report'][i]['FirmwareVersion']
                 exists = json_object['Report'][i]['Detected']
                 failed = json_object['Report'][i]['Failed']
@@ -125,8 +140,75 @@ def PowerSupplies(json_object):
                 
                 host_collection = db[host]
 
+                if fan_failed:
+                        alert_search = host_collection.find({"Category": "PowerSupplies", "Name": name}, sort[('_id', -1)], limit=1)
+                        if alert_search.count() == 0:
+                                slack_alert_message = "Fan {} failed on {}".format(name, host)
+                                slack.alert("PowerSupplies", slack_alert_message)
+                                mongo_doc = {"Name": name, "Category": "PowerSupplies", "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_failed, "FirmwareVersion": fw_ver, "ACOn": ac_on, "Alert": "True"}
+                                host_collection.insert(mongo_doc)
+                        else:
+                                try:
+                                        for item in alert_search:
+                                                if item['Alert'] == "False":
+                                                        slack_alert_message = "Fan {} failed on {}".format(name, host)
+                                                        slack.alert("PowerSupplies", slack_alert_message)
+                                                        mongo_doc = {"Name": name, "Category": "PowerSupplies", "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_failed, "FirmwareVersion": fw_ver, "ACOn": ac_on, "Alert": "True"}
+                                                        host_collection.insert(mongo_doc)
+                                                else:
+                                                        mongo_doc = {"Name": name, "Category": "PowerSupplies", "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_failed, "FirmwareVersion": fw_ver, "ACOn": ac_on, "Alert": "True"}
+                                                        host_collection.insert(mongo_doc)
+                                except Exception as e:
+                                        print(str(e))
+                                        
 
-                mongo_doc = {"Name": name, "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_ok, "FirmwareVersion": fw_ver, "ACOn": ac_on}
+                elif failed:
+                        alert_search = host_collection.find({"Category": "PowerSupplies", "Name": name}, sort[('_id', -1)], limit=1)
+                        if alert_search.count() == 0:
+                                slack_alert_message = "PowerSupply {} failed on {}".format(name, host)
+                                slack.alert("PowerSupplies", slack_alert_message)
+                                mongo_doc = {"Name": name, "Category": "PowerSupplies", "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_failed, "FirmwareVersion": fw_ver, "ACOn": ac_on, "Alert": "True"}
+                                host_collection.insert(mongo_doc)
+                        else:
+                                try:
+                                        for item in alert_search:
+                                                if item['Alert'] == "False":
+                                                        slack_alert_message = "PowerSupply {} failed on {}".format(name, host)
+                                                        slack.alert("PowerSupplies", slack_alert_message)
+                                                        mongo_doc = {"Name": name, "Category": "PowerSupplies", "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_failed, "FirmwareVersion": fw_ver, "ACOn": ac_on, "Alert": "True"}
+                                                        host_collection.insert(mongo_doc)
+                                                else:
+                                                        mongo_doc = {"Name": name, "Category": "PowerSupplies", "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_failed, "FirmwareVersion": fw_ver, "ACOn": ac_on, "Alert": "True"}
+                                                        host_collection.insert(mongo_doc)
+                                except Exception as e:
+                                        print(str(e))
+                elif predict_fail:
+                        alert_search = host_collection.find({"Category": "PowerSupplies", "Name": name}, sort[('_id', -1)], limit=1)
+                        if alert_search.count() == 0:
+                                slack_alert_message = "PowerSupply {} predicted to fail on {}".format(name, host)
+                                slack.alert("PowerSupplies", slack_alert_message)
+                                mongo_doc = {"Name": name, "Category": "PowerSupplies", "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_failed, "FirmwareVersion": fw_ver, "ACOn": ac_on, "Alert": "True"}
+                                host_collection.insert(mongo_doc)
+                        else:
+                                try:
+                                        for item in alert_search:
+                                                if item['Alert'] == "False":
+                                                        slack_alert_message = "PowerSupply {} predicted to fail on {}".format(name, host)
+                                                        slack.alert("PowerSupplies", slack_alert_message)
+                                                        mongo_doc = {"Name": name, "Category": "PowerSupplies", "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_failed, "FirmwareVersion": fw_ver, "ACOn": ac_on, "Alert": "True"}
+                                                        host_collection.insert(mongo_doc)
+                                                else:
+                                                        mongo_doc = {"Name": name, "Category": "PowerSupplies", "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_failed, "FirmwareVersion": fw_ver, "ACOn": ac_on, "Alert": "True"}
+                                                        host_collection.insert(mongo_doc)
+                                except Exception as e:
+                                        print(str(e))
+                
+                else:
+                        mongo_doc = {"Name": name, "Category": "PowerSupplies", "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_failed, "FirmwareVersion": fw_ver, "ACOn": ac_on, "Alert": "False"}
+                        host_collection.insert(mongo_doc)
+
+
+                mongo_doc = {"Name": name, "Detected": exists, "InputRating": input_rating, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_failed, "FirmwareVersion": fw_ver, "ACOn": ac_on}
                 #mongo_doc = {"Name": name, "Detected": exists, "Failed": failed, "PredictedFail": predict_fail, "ACLost": ac_status, "FanFailed": fan_ok, "FirmwareVersion": fw_ver, "ACOn": ac_on}
                 host_collection = db[host].insert_one(mongo_doc)
 
