33fabae4700d12109ea570c696c8e80af638388a
mitchya1 <mitchell.anderson@colorado.edu>
2019-02-17 11:25:16 -0700

    fix incorrect import and app.config call

--- domsa_web/backends/domas_statsd.py
+++ domsa_web/backends/domas_statsd.py
@@ -1,9 +1,9 @@
 import statsd
-from domsa_web.app import config
+from domsa_web import app
 
 def report_good_metrics(server):
-    s_client = statsd.StatsClient(config['STATSD_SERVER'], config['STATSD_PORT'], prefix='good')
+    s_client = statsd.StatsClient(app.config['STATSD_SERVER'], app.config['STATSD_PORT'], prefix='good')
     s_client.incr(server)
 def report_bad_metrics(server):
-    s_client = statsd.StatsClient(config['STATSD_SERVER'], config['STATSD_PORT'], prefix='bad')
+    s_client = statsd.StatsClient(app.config['STATSD_SERVER'], app.config['STATSD_PORT'], prefix='bad')
     s_client.incr(server)
